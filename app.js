42a72483224ad245e039623fe1b8436910dd698f
vinoth <a.vinothk@outlook.com>
2015-08-18 18:53:16 +0530

    include phantom and the scraper

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var app = express();
+var phantom = require('phantom');
 
 app.set('port', (process.env.PORT || 5000));
 
 
-app.get('/', function(request, response) {
-  response.send('hello universe');
+app.get('/fetch', function(request, response) {
+  url = request.query.url
+  phantom.create(function (ph) {
+    ph.createPage(function (page) {
+      page.open(url, function (status) {
+        page.evaluate(function () { return document.documentElement.outerHTML; }, function (result) {
+          response.header('Content-Type', 'text/plain');
+          response.send(String(result));
+          ph.exit();
+        });
+      });
+    });
+  });
 });
 
 app.listen(app.get('port'), function() {
